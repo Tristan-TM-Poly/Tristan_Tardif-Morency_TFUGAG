04601f59e38d1100352619009725cb91ff138b1c
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-06-13 17:26:00 -0400

    t

--- tfuga_library/src/tfuga/__init__.py
+++ tfuga_library/src/tfuga/__init__.py
@@ -1,3 +1,21 @@
+from .algebra import AlgebraRoute, TGFAAlgebraRouter
+from .cvcd import CVCDResidue, OmniCVCDExtractor
 from .engine import TFUGAEngine
+from .github_orchestrator import GitHubActivationPlan, GitHubOrchestrator
+from .hgfm import HGFMBuilder, HGFMGraph
+from .memory import MemoryEdge, MemoryGraph, MemoryNode
+from .miner import GraphProblemMiner, ProblemSeed
+from .oak import OAKGate, OAKResult
+from .proposals import Proposal, ProposalChange, ProposalValidation, ProposalValidator
+from .runner import Runner, RunResult
+from .statebook import StateBook, StateStep
+from .writer import TextPublisher, TextSection
 
-__all__ = ["TFUGAEngine"]
+__all__ = [
+    "AlgebraRoute", "TGFAAlgebraRouter", "CVCDResidue", "OmniCVCDExtractor",
+    "TFUGAEngine", "GitHubActivationPlan", "GitHubOrchestrator", "HGFMBuilder",
+    "HGFMGraph", "MemoryEdge", "MemoryGraph", "MemoryNode", "GraphProblemMiner",
+    "ProblemSeed", "OAKGate", "OAKResult", "Proposal", "ProposalChange",
+    "ProposalValidation", "ProposalValidator", "Runner", "RunResult", "StateBook",
+    "StateStep", "TextPublisher", "TextSection"
+]
