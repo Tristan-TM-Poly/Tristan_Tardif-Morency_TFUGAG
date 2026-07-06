cea5d781559d1233ef53f2c968b6f0c3be3843d1
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-07-06 18:02:57 -0400

    Add OAKShield Core placeholder package

---
+++ tools/oakshield_core/__init__.py
@@ -0,0 +1,7 @@
+"""OAKShield core placeholder package.
+
+This repository starts with documentation-only OAKShield Core hardening.
+Executable helpers can be added later in reviewed PRs.
+"""
+
+__all__ = []
